4a9860697285c641524b3977fe487f819f2c2792
Fernando Rojas <frojas@monchis.com.py>
2024-06-01 18:32:20 +0000

    fix: database

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -4,13 +4,13 @@ import jwt from "jsonwebtoken";
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "Auth failed. No token provided." });
+    return res.status(401).json({ data: {}, message: "Auth failed. No token provided.", error: true });
   }
   try {
     const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET || "");
     req.body.userId = decodedToken.userId;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Auth failed. Invalid token." });
+    return res.status(401).json({ data: {}, message: "Auth failed. Invalid token.", error: true });
   }
 };
